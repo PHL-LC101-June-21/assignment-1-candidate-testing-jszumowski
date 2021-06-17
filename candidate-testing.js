d178ae6c4dc95f6ef892a35864d94b503a2bc416
Joseph Szumowski <85529714+jszumowski@users.noreply.github.com>
2021-06-17 18:22:36 +0000

    I took my best shot at Part 2 of Assignment 1. I'm hoping Class 3 sheds some light on the... messier parts of my code.

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,32 +5,53 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? "
-let correctAnswer = "Sally Ride"
-let candidateAnswer = " "
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let question;
+let correctAnswer;
+let candidateAnswer;
+let candidateAnswers = [];
+let candidateAnswers1;
+let candidateAnswers2;
+let candidateAnswers3;
+let candidateAnswers4;
+let candidateAnswers5;
+let correctAnswers = 0;
+let questions = ['Who was the first American woman in space? ', 'Ture or False: 5 kilometers = 5000 meters. ', '(5+3)/2*10 = ', 'Given the array [8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
+let answers = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  // I don't understand why you want is to ask for the candidateName twice. I asked for candidateName in part 1.1c because that's where it works. 
-  //I'm going to ask about this during class today, and if I figure out why we are prompted to ask for candidateName twice, I will fix it and resubmit the assignment. Otherwise, the code operates the way it's supposed to so... 
+ let candidateName = input.question("Enter your name: ");
+  console.log("Hello, " + candidateName);
+  // I figured it out! So, this is a function that we write here and then call upon later in the code to run! 
 }
 
-function askQuestion() {
+//function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  //let candidateAnswer = input.question(question);
+    //if (candidateAnswer.tolowerCase === correctAnswer.tolowerCase){
+     // console.log("That is correct");
+   // } else {
+    //  console.log("That is not correct. Please learn astrofeminism and try again next year.");
+    //}
+//}
 
-
+function askQuestions() { 
+  let candidateAnswers1 = input.question(questions[0]);
+  let candidateAnswers2 = input.question(questions[1]);
+  let candidateAnswers3 = input.question(questions[2]);
+  let candidateAnswers4 = input.question(questions[3]);
+  let candidateAnswers5 = input.question(questions[4]);
+  let candidateAnswers = [candidateAnswers1, candidateAnswers2, candidateAnswers3, candidateAnswers4, candidateAnswers5];
+  //I feel like theres a cleaner way to do this? Like these 6 lines of code could be 1. 
+  console.log(`Your answers: ${candidateAnswers}`,`\nCorrect answers: ${answers}`)
 }
+// I'm not sure if this is what they want this to look like? I would love to see the finished program and 
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
+  
+  let grade
   
 
   return grade;
@@ -38,16 +59,8 @@ function gradeQuiz(candidateAnswers) {
 
 function runProgram() {
   askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  let candidateName = input.question("Enter your name: ");
-  console.log("Hello, " + candidateName)
-  askQuestion();
-  let candidateAnswer = input.question(question)
-    if (candidateAnswer === correctAnswer){
-      console.log("That is correct")
-    } else {
-      console.log("That is not correct. Please learn astrofeminism and try again next year.")
-    }
+  askQuestions();
+  gradeQuiz();
 }
 
 // Don't write any code below this line //
